48911b32b6756a5bade92a7e74cb4af75bb6d4e7
janKaje <66396673+janKaje@users.noreply.github.com>
2020-06-15 11:59:00 -0500

    testing

--- salana.py
+++ salana.py
@@ -11,6 +11,7 @@ client.remove_command('help')
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game('type ,help for help'))
+    await client.get_channel(705223622981320706).send(time.strftime('Began running on %A, %d %B %Y, %H:%M:%S UTC', time.gmtime()))
     await client.get_user(client.owner_id).send(time.strftime('Began running on %A, %d %B %Y, %H:%M:%S UTC', time.gmtime()))
 
 #Load, unload, reload cog commands
