c9e49228da4598a455eb087d516e4a16c5db3efd
janKaje <66396673+janKaje@users.noreply.github.com>
2020-07-11 23:37:40 -0500

    patch and update
    updated .gitignore and fixed a small error in ,random

--- cogs/fun.py
+++ cogs/fun.py
@@ -120,7 +120,7 @@ class fun(commands.Cog):
         value = 10**(random.random()*12) #gets value
         highscore_raw = open('/app/value.txt').read()
         highscore_user = re.sub(r':\d*', '', highscore_raw, flags=re.DOTALL)
-        highscore_value = int(re.sub('[^:]*:', '', highscore_raw, flags=re.DOTALL))
+        highscore_value = int(re.sub('[^:]*:|\n', '', highscore_raw, flags=re.DOTALL))
         if value > highscore_value:
             embed = discord.Embed(color=discord.Color.blurple(), title='Congratulations!! :partying_face:', description='You beat the high score!')
             embed.add_field(name='Old high score:', value=f'{highscore_user} got {highscore_value}')
